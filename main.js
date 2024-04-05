c5bdcf5856c95b86d01310c6c1f4129dbc9978ae
Asadkhan12334455 <160310609+Asadkhan12334455@users.noreply.github.com>
2024-04-05 18:37:12 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+"use strict";
+// let alien_color:string = "blue";
+// if (alien_color == "blue") {
+//     console.log("You just earned 5 points!");
+// }
+// let alien_color = "green";
+// if (alien_color == "green") {
+//     console.log("You just earned 5 points for shooting the alien!");
+// } else {
+//     console.log("You just earned 10 points.");
+// }
+let alien_color = "yellow";
+if (alien_color == "green") {
+    console.log("You just earned 5 points for shooting the alien!");
+}
+else {
+    console.log("You just earned 10 points.");
+}
